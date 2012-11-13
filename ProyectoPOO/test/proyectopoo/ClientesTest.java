1034d2c77dcba7a73d7d91b0fa2beadb03d15480
Walterfuentes <wfuentesb@gmail.com>
2012-11-13 09:26:28 -0500

    Clase Clientes y test para las historias

---
+++ ProyectoPOO/test/proyectopoo/ClientesTest.java
@@ -0,0 +1,14 @@
+package proyectopoo;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author walterfuentes
+ */
+public class ClientesTest {
+    
+}
