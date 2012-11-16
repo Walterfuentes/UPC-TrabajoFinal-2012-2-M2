5922016d30f4221e6ffe15ffb81f982b5c89429b
Walterfuentes <wfuentesb@gmail.com>
2012-11-16 07:43:12 -0500

    Creación de Cliente para el uso en Prospecto y Alta Cliente

--- ProyectoPOO/src/proyectopoo/Cliente.java
+++ ProyectoPOO/src/proyectopoo/Cliente.java
@@ -4,6 +4,7 @@
  */
 package proyectopoo;
 
+ import java.util.Date;
 /**
  *
  * @author walterfuentes
@@ -13,6 +14,86 @@ public class Cliente {
     private String nombre;
     private String ApellidoPaterno;
     private String ApellidoMaterno;
+    private String Correo;
+    private int Dni;
+    private int telefonofijo;
+    private int celular;
+    private java.util.Date fechaIngreso;
+
+    public Cliente(String nombre, String ApellidoPaterno, String ApellidoMaterno, String Correo, int Dni, int telefonofijo, int celular) {
+        this.nombre = nombre;
+        this.ApellidoPaterno = ApellidoPaterno;
+        this.ApellidoMaterno = ApellidoMaterno;
+        this.Correo = Correo;
+        this.Dni = Dni;
+        this.telefonofijo = telefonofijo;
+        this.celular = celular;
+        this.fechaIngreso = new java.util.Date();
+    }
+
+    public String getApellidoMaterno() {
+        return ApellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String ApellidoMaterno) {
+        this.ApellidoMaterno = ApellidoMaterno;
+    }
+
+    public String getApellidoPaterno() {
+        return ApellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String ApellidoPaterno) {
+        this.ApellidoPaterno = ApellidoPaterno;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String Correo) {
+        this.Correo = Correo;
+    }
+
+    public int getDni() {
+        return Dni;
+    }
+
+    public void setDni(int Dni) {
+        this.Dni = Dni;
+    }
+
+    public int getCelular() {
+        return celular;
+    }
+
+    public void setCelular(int celular) {
+        this.celular = celular;
+    }
+
+    public Date getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void setFechaIngreso(Date fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTelefonofijo() {
+        return telefonofijo;
+    }
+
+    public void setTelefonofijo(int telefonofijo) {
+        this.telefonofijo = telefonofijo;
+    }
     
     
 }
