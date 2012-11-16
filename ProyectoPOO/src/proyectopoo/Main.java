9ebffb36062f72f2fd15a80f0b22133f4152b630
Walterfuentes <wfuentesb@gmail.com>
2012-11-16 06:51:53 -0500

    actualizando código

---
+++ ProyectoPOO/src/proyectopoo/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectopoo;
+
+/**
+ *
+ * @author walterfuentes
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+
+    }
+    
+}
---
+++ ProyectoPOO/test/proyectopoo/ClienteTest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectopoo;
+
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+/**
+ *
+ * @author walterfuentes
+ */
+public class ClienteTest {
+    
+
+    public ClienteTest () {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+
+}
