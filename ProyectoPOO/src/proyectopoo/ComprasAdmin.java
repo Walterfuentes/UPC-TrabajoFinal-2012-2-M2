344d7098d5cb5bc756911199e5e2f6732fcaa5c5
Carlos Ricaldi <carlosricaldif@gmail.com>
2012-11-18 23:13:40 -0500

    Creando ComprasAdmin
    Creando ComprasAdmin

---
+++ ProyectoPOO/src/proyectopoo/ComprasAdmin.java
@@ -0,0 +1,19 @@
+
+/*
+ * Historia de Usuario: ComprasAdmin
+ * Desarrollado: Carlos Ricaldi
+ */
+package proyectopoo;
+
+import java.util.ArrayList;
+
+public class ComprasAdmin {
+    private ArrayList<Compras> compras;
+
+    public ComprasAdmin() {
+        compras = new ArrayList<Compras>();
+    }
+    
+    
+    
+}
