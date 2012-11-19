eebdf8098f109f233e63ab9e9890008a1b0db661
Anyelo Menacho Flores <anyelo.menacho@gmail.com>
2012-11-18 23:57:06 -0500

    Obtener Datos Clase Cliente

--- ProyectoPOO/src/proyectopoo/Prospecto.java
+++ ProyectoPOO/src/proyectopoo/Prospecto.java
@@ -4,10 +4,22 @@
  */
 package proyectopoo;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author anyelomenachoflores
  */
 public class Prospecto {
     
+    private ArrayList<Cliente> clientes;
+    
+    public Prospecto() {
+        clientes=new ArrayList<Cliente>();
+    }
+
+    public void obtenerDatosProspecto(String nombre, String ApellidoPaterno, String ApellidoMaterno, String Correo, String Dni, String telefonofijo, String celular) {
+        Cliente cliente=new Cliente(nombre, ApellidoPaterno, ApellidoMaterno, Correo, Dni, telefonofijo, celular);
+        clientes.add(cliente);
+    }
 }
