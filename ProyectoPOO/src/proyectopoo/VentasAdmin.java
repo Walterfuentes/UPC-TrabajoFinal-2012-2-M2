a2c4f0d63d9e9abd87f2b3fa0243dc52c8d2f7b9
Pedro Balarezo <pj.balarezo.hernandez@gmail.com>
2012-11-20 00:55:52 -0500

    Implementando Clase VentasAdmin Optimizando codigo

---
+++ ProyectoPOO/src/proyectopoo/VentasAdmin.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectopoo;
+
+/**
+ * @author Pedro Balarezo
+ */
+public class VentasAdmin {
+    
+}
