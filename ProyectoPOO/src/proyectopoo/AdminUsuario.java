0410f36468db702327f1010e2be03b1cbe98b491
Pedro Balarezo <pj.balarezo.hernandez@gmail.com>
2012-11-21 01:41:46 -0500

    Crear Clase AdminUsuarios

---
+++ ProyectoPOO/src/proyectopoo/AdminUsuario.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectopoo;
+
+/**
+ *
+ * @author Pedro Balarezo
+ */
+public class AdminUsuario {
+
+    public void Buscar(String usuario) {
+    }
+
+    public void Adicionar() {
+    }
+
+    public void Editar() {
+    }
+
+    public void Eliminar() {
+    }
+
+    public void ValidarUsuario() {
+    }
+}
