b9f620d24be7db764de61a78cb9c031f9387ad42
Pedro Balarezo <pj.balarezo.hernandez@gmail.com>
2012-11-23 15:57:31 -0500

    Optimizando Clase HistoriaVentasTest

--- ProyectoPOO/test/proyectopoo/HistoriaVentaTest.java
+++ ProyectoPOO/test/proyectopoo/HistoriaVentaTest.java
@@ -15,35 +15,38 @@ public class HistoriaVentaTest {
     @Test
     public void validarAltaVenta() {
 
-        String concepto = "Libros";
-        int numero = 10;
-        int fecha_emision = 200121012;
-        String empresa = "empresa1";
-        double subtotal = 500;
-        double igv = 512;
-        double total = 5486;
-        String moneda = "soles";
-        int fecha_vencimiento = 200121012;
-        int fecha_pago = 200121012;
-        String observaciones = "provando";
+    String factura_boleta="0001";
+    String concepto="Libros";
+    int numero=10;
+    int fecha_emision=20121012;
+    String empresa="empresa1";
+    double subtotal=500;
+    double igv=512;
+    double total=5486;
+    String moneda="soles";
+    int fecha_vencimiento=20121512;
+    int fecha_pago=20121612;
+    String estado="bien";
+    String observaciones="provando";
 
-//       Venta v = new Venta(factura_boleta, concepto, numero, fecha_emision, empresa, subtotal, igv, total, moneda, fecha_vencimiento, fecha_pago, observaciones, moneda);
+     Venta v = new Venta(factura_boleta, concepto, numero, fecha_emision, empresa, subtotal, igv, total, moneda, fecha_vencimiento, fecha_pago, observaciones, moneda);
 ////       VentasAdmin vd = new VentasAdmin();
 //
 ////       vd.Alta(factura_boleta, concepto, numero, fecha_emision, empresa, subtotal, igv, total, moneda, fecha_vencimiento, fecha_pago, observaciones, moneda);
 //
 ////
 ////
-//        assertNotNull(v);
-//        assertEquals(factura_boleta, v.getFactura_boleta());
-//        assertEquals(concepto, v.getConcepto());
-//        assertEquals(fecha_emision, v.getFecha_emision());
-//        assertEquals(fecha_vencimiento, v.getFecha_vencimiento());
-//        assertEquals(subtotal, v.getSubtotal(),0.0);
-//        assertEquals(igv, v.getIgv(), 0.0);
-//        assertEquals(total, v.getTotal(), 0.0);
-//        assertEquals(moneda, v.getMoneda());
-//        assertEquals(observaciones, v.getObservaciones());
-//        
+        
+        assertNotNull(v);
+        assertEquals(factura_boleta, v.getFactura_boleta());
+        assertEquals(concepto, v.getConcepto());
+        assertEquals(fecha_emision, v.getFecha_emision());
+        assertEquals(fecha_vencimiento, v.getFecha_vencimiento());
+        assertEquals(subtotal, v.getSubtotal(),0.0);
+        assertEquals(igv, v.getIgv(), 0.0);
+        assertEquals(total, v.getTotal(), 0.0);
+        assertEquals(moneda, v.getMoneda());
+        assertEquals(observaciones, v.getObservaciones());
+        
     }
 }
