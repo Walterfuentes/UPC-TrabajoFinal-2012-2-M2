7fca827fdea083d2437729b93de64c8f50b9ecd0
Walter Fuentes <wfuentesb@gmail.com>
2012-11-28 07:23:21 -0500

    Prospecto

--- ProyectoPOO/test/proyectopoo/ADMINProspectoTest.java
+++ ProyectoPOO/test/proyectopoo/ADMINProspectoTest.java
@@ -4,12 +4,10 @@
  */
 package proyectopoo;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 import org.junit.Test;
-import org.junit.AfterClass;
 import static org.junit.Assert.*;
-import org.junit.BeforeClass;
+
 
 /**
 
@@ -21,40 +19,6 @@ public class AdminProspectoTest {
     public AdminProspectoTest() {
     }
     
-//   
-//    @Test
-//    public void ValidarDatosAlRegistrar() {
-//        // Arrange
-//        String dni = "";
-//        String apellidoPaterno = "Menacho";
-//        String apellidoMaterno = "";
-//        String nombre = "Anyelo";
-//        String correo = "";
-//        String telefonofijo = "";
-//        String celular="";
-//        String fecha = "081212";
-//        AltaProspecto admin = new AltaProspecto();        
-//        admin.registrarProspecto(dni, apellidoPaterno, apellidoMaterno, nombre, correo, telefonofijo, celular, fecha);
-//    }
-//    @Test
-//    public void ValidarDuplicidad() {
-//        // Arrange
-//        String dni = "23272090";
-//        String apellidoPaterno = "Menacho";
-//        String apellidoMaterno = "Flores";
-//        String nombre = "Anyelo";
-//        String correo = "amenacho@atv.com.pe";
-//        String telefonofijo = "4567890";
-//        String celular="987654345";
-//         String fecha = "081212";
-//        AltaProspecto admin = new AltaProspecto();
-//        // Act
-//        admin.registrarProspecto(dni, apellidoPaterno, apellidoMaterno, nombre, correo, telefonofijo, celular,fecha);
-//         
-//        admin.registrarProspecto(dni, apellidoPaterno, apellidoMaterno, nombre, correo, telefonofijo, celular, fecha);
-//        // Assert
-//        // No se necesita comprobacion por que se espera que suceda
-//        
-//   }
+
 }    
   
