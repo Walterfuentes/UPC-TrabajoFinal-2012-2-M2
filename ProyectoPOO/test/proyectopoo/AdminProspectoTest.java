524c2126a119b67e44fa9f4c6ce18266ef2c038d
Walter Fuentes <wfuentesb@gmail.com>
2012-11-28 07:27:28 -0500

    Corriegiendo Prospectos

---
+++ ProyectoPOO/test/proyectopoo/AdminProspectoTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectopoo;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author walterfuentes
+ */
+public class AdminProspectoTest {
+    
+    public AdminProspectoTest() {
+    }
+
+   
+}
