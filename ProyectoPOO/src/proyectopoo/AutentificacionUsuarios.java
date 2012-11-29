23b7d437bbb64d3a07b8cbfa11fb02e0ac2d15f3
Anyelo Menacho Flores <anyelo.menacho@gmail.com>
2012-11-29 10:01:51 -0500

    Creando Clase AutentificacionUsuarios

---
+++ ProyectoPOO/src/proyectopoo/AutentificacionUsuarios.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectopoo;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author AnyeloMenacho
+ */
+public class AutentificacionUsuarios {
+    
+    private ArrayList<Usuarios> usuarios;
+
+    /*public AutentificacionUsuarios(String dni, String nombre, String apellidoPaterno, String apellidoMaterno, String usuario, String correo, String fechaIngreso, String cargo, String rol, String contrasenia) {
+        super(dni, nombre, apellidoPaterno, apellidoMaterno, usuario, correo, fechaIngreso, cargo, rol, contrasenia);
+    }
+    *
+    */
+    public AutentificacionUsuarios() {
+        usuarios = new ArrayList<Usuarios>();
+    }
+    
+    
+    
+    
+}
