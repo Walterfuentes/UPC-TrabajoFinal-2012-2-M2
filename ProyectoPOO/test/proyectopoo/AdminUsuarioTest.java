22a09f4c399e371ec27f3ba5fdc36f1dee3ea892
Pedro Balarezo <pj.balarezo.hernandez@gmail.com>
2012-11-30 02:42:35 -0500

    Optimizando Clase  AdminUsuarios TEST - Metodo Adicionar usuarios

--- ProyectoPOO/test/proyectopoo/AdminUsuarioTest.java
+++ ProyectoPOO/test/proyectopoo/AdminUsuarioTest.java
@@ -10,30 +10,40 @@ import static org.junit.Assert.*;
 /**
  *
  * @author walterfuentes
+ * @author PedroBalarezo
  */
 public class AdminUsuarioTest {
     
-    public AdminUsuarioTest() {
-    }
+     @Test
+   public void DebeRegistrarUsuario (){
+     String Dni="123456789";
+     String Nombre="Gianina";
+     String ApellidoPaterno="Quispe";
+     String ApellidoMaterno="Rosas";
+     String Usuario="u0001";
+     String Correo="123@gmail.com";
+     String FechaIngreso="10/10/2012";
+     String Cargo="jefe";
+     String Rol="rol1";
+     String Contrasenia="1234";
+     
+     AdminUsuario adminUsuario=new AdminUsuario();
+     adminUsuario.Adicionar(Dni, Nombre, ApellidoPaterno, ApellidoMaterno, Usuario, Correo, FechaIngreso, Cargo, Rol, Contrasenia);
+   
+     Usuarios elemento = adminUsuario.Buscar(Dni);
+     assertNotNull(elemento);
+     assertEquals(Dni,elemento.getDni());
+     assertEquals(Nombre,elemento.getNombre());
+     assertEquals(ApellidoPaterno,elemento.getApellidoPaterno());
+     assertEquals(ApellidoMaterno,elemento.getApellidoMaterno());
+     assertEquals(Usuario,elemento.getUsuario());
+     assertEquals(Correo,elemento.getCorreo());
+     assertEquals(FechaIngreso,elemento.getFechaIngreso());
+     assertEquals(Cargo,elemento.getCargo());
+     assertEquals(Rol,elemento.getRol());
+     assertEquals(Contrasenia,elemento.getContrasenia());
+     
+     
+   }
 
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
-    @Test
-    public void testSomeMethod() {
-        fail("The test case is a prototype.");
-    }
 }
